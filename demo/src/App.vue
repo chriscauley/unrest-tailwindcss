77ac9a52f932fd94c263efbd04ddd69a19225012
chriscauley <chris@lablackey.com>
2022-06-18 17:28:50 -0400

    button view

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>Hey</div>
+  <div>
+    <button-view />
+  </div>
 </template>
 
 <script>
+import ButtonView from '@/components/Buttons.vue'
+
+export default {
+  components: { ButtonView },
+}
 </script>
---
+++ demo/src/components/Buttons.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-for="button in buttons" :key="button" class="story-row">
+      <div :class="button">{{ button }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+const button_types = [
+  'primary',
+  'secondary',
+  'success',
+  'danger',
+  'error',
+  'warning',
+  'info',
+  'light',
+  'dark',
+  'text',
+  'link',
+]
+
+export default {
+  computed: {
+    buttons() {
+      return button_types.map(t => `btn -${t}`)
+    }
+  }
+}
+</script>
+
+<style>
+.story-row {
+  margin-bottom: 0.5rem;
+}
+</style>
